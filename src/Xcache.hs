6c0a7aa6968a53ac612e44a71becad89ab36abc9
Pascal Honoré <pascal@alseyn.net>
2015-12-03 19:45:49 +0000

    updating from pascal

--- src/Xcache.hs
+++ src/Xcache.hs
@@ -3,10 +3,10 @@ module Xcache (
     get
 ) where
 
-import Data.Digest.Pure.SHA as SHA
-import Data.ByteString.Lazy.Char8 as Char8
-import qualified System.Directory as Dir
-import qualified Data.Time.Clock.POSIX as Time
+import           Data.ByteString.Lazy.Char8 as Char8
+import           Data.Digest.Pure.SHA       as SHA
+import qualified Data.Time.Clock.POSIX      as Time
+import qualified System.Directory           as Dir
 
 -- Data.Digest.Pure.SHA
 --     sha1 :: Data.ByteString.Lazy.Internal.ByteString -> Digest SHA1State
@@ -18,25 +18,38 @@ import qualified Data.Time.Clock.POSIX as Time
 getCurrentUnixTime :: IO Int
 getCurrentUnixTime = round `fmap` Time.getPOSIXTime
 
-keyToDataFilepathEnsureParentFolder :: String -> IO String
-keyToDataFilepathEnsureParentFolder key = do
-    let filename = "sha1-" ++ ( SHA.showDigest $ SHA.sha1 $ Char8.pack key )
-    let fragment1 = Prelude.take 2 ( Prelude.drop 5 filename )
-    let fragment2 = Prelude.take 2 ( Prelude.drop 7 filename )
-    let folderpath = "/x-space/xcache-v2/datablobs/"++fragment1++"/"++fragment2
-    let filepath = folderpath++"/"++filename
+getDigestAndFragments :: String -> (String, String, String)
+getDigestAndFragments key = let
+        digest = SHA.showDigest $ SHA.sha1 $ Char8.pack key
+        fragment1 = Prelude.take 2 digest
+        fragment2 = Prelude.take 2 fragment1
+    in (digest, fragment1, fragment2)
+
+ensureAndGetFolderPathWithPrefix :: String -> String -> String -> IO String
+ensureAndGetFolderPathWithPrefix p f f' =
+    let folderpath = p ++ "/" ++ f ++ "/" ++ f' in
     Dir.createDirectoryIfMissing True folderpath
-    return filepath
+    >> return folderpath
+
+keyToDataFilepathEnsureParentFolder :: String -> IO String
+keyToDataFilepathEnsureParentFolder key = let
+        (digest, fragment1, fragment2) = getDigestAndFragments key
+        filename = "sha1-" ++ digest
+    in do
+        folderpath <- ensureAndGetFolderPathWithPrefix
+                        "/x-space/xcache-v2/datablobs" fragment1 fragment2
+        let filepath = folderpath ++ "/" ++ filename
+        return filepath
 
 keyToTimestampFilepathEnsureParentFolder :: String -> IO String
-keyToTimestampFilepathEnsureParentFolder key = do
-    let filename = "sha1-" ++ ( SHA.showDigest $ SHA.sha1 $ Char8.pack key )
-    let fragment1 = Prelude.take 2 ( Prelude.drop 5 filename )
-    let fragment2 = Prelude.take 2 ( Prelude.drop 7 filename )
-    let folderpath = "/x-space/xcache-v2/timestamps/"++fragment1++"/"++fragment2
-    let filepath = folderpath++"/"++filename
-    Dir.createDirectoryIfMissing True folderpath
-    return filepath
+keyToTimestampFilepathEnsureParentFolder key = let
+        (digest, fragment1, fragment2) = getDigestAndFragments key
+        filename = "sha1-" ++ digest
+    in do
+        folderpath <- ensureAndGetFolderPathWithPrefix
+                        "/x-space/xcache-v2/timestamps" fragment1 fragment2
+        let filepath = folderpath ++ "/" ++ filename
+        return filepath
 
 set :: String -> String -> IO ()
 set key value = do
@@ -48,19 +61,14 @@ set key value = do
     return ()
 
 get :: String -> IO String
-get key = do 
-    filepath <- keyToDataFilepathEnsureParentFolder key 
+get key = do
+    filepath <- keyToDataFilepathEnsureParentFolder key
     fileExists <- Dir.doesFileExist filepath
     if fileExists
         then do
             timestampfilepath <- keyToTimestampFilepathEnsureParentFolder key
             currenttime <- getCurrentUnixTime
             Prelude.writeFile timestampfilepath $ show currenttime
-            contents <- Prelude.readFile filepath  
-            return contents 
-        else do
+            Prelude.readFile filepath
+        else
             return ""
-
-
-
-
