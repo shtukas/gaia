3302da562b9568b46c8b5bd61e8cef69485105f7
Pascal Honoré <pascal@alseyn.net>
2015-12-08 11:00:55 +0000

    Xcache update (small)

--- src/PStorageServices/ContentAddressableStore.hs
+++ src/PStorageServices/ContentAddressableStore.hs
@@ -7,13 +7,18 @@ import qualified PStorageServices.Xcache as X
 import qualified Data.ByteString.Lazy.Char8 as Char8
 import           Data.Digest.Pure.SHA       as SHA
 
-set :: Char8.ByteString -> IO String
-set value = 
-    let key1 = "24f4da84-5d85-4933-8775-dc6593dcad04:" ++ ( SHA.showDigest $ SHA.sha1 value )
+
+makeKeyForValue :: Char8.ByteString -> String
+makeKeyForValue bytestring = 
+    let key1 = "24f4da84-5d85-4933-8775-dc6593dcad04:" ++ ( SHA.showDigest $ SHA.sha1 bytestring )
         key2 = SHA.showDigest $ SHA.sha1 $ Char8.pack key1 
-    in do 
-        X.set ( key2 ) value
-        return key2
+    in key2
+
+set :: Char8.ByteString -> IO String
+set bytestring = do 
+    let key = makeKeyForValue bytestring
+    X.set key bytestring
+    return key
 
 get :: String -> IO ( Maybe Char8.ByteString )
 get key = X.get key
