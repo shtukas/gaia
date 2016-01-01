e6decddf6ad28cad12ff410cf9e5dbc0cd92783f
Pascal Honoré <pascal@alseyn.net>
2016-01-01 01:05:07 +0000

    Update in GaiaWeb.hs
    We now have a correct mime type set against HTTP replies of api/v1/search.
    Example:
    
    [pascal@alexandra Desktop]
    $ curl -I http://localhost:8000/api/v1/search/pascal
    HTTP/1.1 200 OK
    Transfer-Encoding: chunked
    Connection: Keep-Alive
    Content-Type: application/json
    Date: Fri, 01 Jan 2016 01:02:08 GMT
    Server: Happstack/7.4.5

--- app/GaiaWeb.hs
+++ app/GaiaWeb.hs
@@ -3,19 +3,31 @@
 module Main where
 
 import Control.Monad    (msum)
-import Happstack.Server (dirs, path, nullConf, ok, simpleHTTP, serveDirectory, Browsing(EnableBrowsing), toResponse)
+
+import Happstack.Server (dirs, path, nullConf, simpleHTTP, 
+                         serveDirectory, Browsing(EnableBrowsing))
+import Happstack.Server.Response as R
+
 import Text.JSON.Generic
+import qualified Data.ByteString.Char8 as B1
+import qualified Data.ByteString.Lazy.Char8 as Char8
 
--- TODO: remove root directory hardcoding
+data Patterns = PatternsC [String]
 
-alpha_search :: String -> [String]
-alpha_search pattern = ["/Users/pascal/Desktop/alice.txt"]
+instance R.ToMessage Patterns where
+  toContentType _ = B1.pack "application/json"
+  toMessage (PatternsC x) = (Char8.pack . encodeJSON) x
+
+alpha_search :: String -> Patterns
+alpha_search pattern = PatternsC ["/Users/pascal/Desktop/alice.txt"]
 
 main :: IO ()
 main = simpleHTTP nullConf $
     msum [   dirs "api/v0" $ ok $ toResponse "Use the Force!"
-           , dirs "api/v1/search" $ path ( \pattern -> ok $ toResponse $ encodeJSON $ alpha_search pattern )
+           , dirs "api/v1/search" $ do path ( \pattern -> ok ( toResponse $ alpha_search pattern ) )
            , serveDirectory EnableBrowsing ["index.html"] "/Lucille-E/Applications/Gaia/web-root"
          ]
 
+-- currently runs at http://localhost:8000
+-- TODO: remove root directory hardcoding
 
